f9068569ffbf5fc16ede8d4d0863fbc436a87fc8
My Ho <mydiemho@gmail.com>
2019-05-24 11:35:30 -0700

    stash

--- src/plugins/package/azurePackage.ts
+++ src/plugins/package/azurePackage.ts
@@ -1,4 +1,5 @@
-
+import { existsSync, renameSync, statSync } from 'fs';
+import { join } from "path";
 import Serverless from 'serverless';
 import AzureProvider from '../../provider/azureProvider';
 import { BindingUtils } from '../../shared/bindings';
@@ -11,6 +12,7 @@ export class AzurePackage {
   constructor(private serverless: Serverless, private options: Serverless.Options) {
     this.hooks = {
       'package:setupProviderConfiguration': this.setupProviderConfiguration.bind(this),
+      'before:webpack:package:packageModules': this.webpackFunctionJson.bind(this)
     };
   }
 
@@ -26,4 +28,40 @@ export class AzurePackage {
 
     return Promise.all(createEventsPromises);
   }
+
+  private async webpackFunctionJson(): Promise<any> {
+    if (existsSync('.webpack')) {
+      const webpackJsonPromises = this.serverless.service.getAllFunctions()
+        .map((functionName) => this.moveJsonFile(functionName));
+
+      return Promise.all(webpackJsonPromises);
+    }
+
+    return Promise.resolve();
+  }
+
+  private async moveJsonFile(functionName): Promise<any> {
+    const dirPath = join(this.serverless.config.servicePath, '.webpack', functionName);
+    const jsonFileName = `function.json`;
+    const jsonFileSrcPath = join(this.serverless.config.servicePath, functionName, jsonFileName);
+    const jsonFileDestPath = join(dirPath, jsonFileName);
+
+    this.serverless.cli.log(`src: ${jsonFileSrcPath}`);
+    this.serverless.cli.log(`src: ${jsonFileDestPath}`);
+
+    return new Promise((resolve) => {
+      if (existsSync(dirPath) && statSync(dirPath).isDirectory()) {
+        if (existsSync(jsonFileSrcPath)) {
+          this.serverless.cli.log(`Moving ${jsonFileName} to .webpack directory.`);
+          renameSync(jsonFileSrcPath, jsonFileDestPath);
+        }
+        else {
+          this.serverless.cli.log(`Warning: No generated ${functionName}/function.json file was found. It will not be included in the package.`);
+        }
+      }
+
+      resolve();
+    });
+  }
+}
 }
